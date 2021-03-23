449801e3375b917d17f8c422cca0245bc7f0cd21
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2021-03-23 18:54:00 +0100

    found player library

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final buttonColors = [
+    Colors.red,
+    Colors.deepOrange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.deepPurple
+  ];
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Xylophone'),
-          backgroundColor: Colors.teal,
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            children: [
+              for (var i = 1; i <= 7; i += 1)
+                TextButton(
+                  onPressed: () {
+                    AssetsAudioPlayer.newPlayer().open(
+                      Audio("assets/note$i.wav"),
+                      autoStart: true,
+                      showNotification: true,
+                    );
+                    print("Clicked $i button");
+                  },
+                  child: Text('Click me'),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(buttonColors[i - 1]),
+                  ),
+                )
+            ],
+          ),
         ),
       ),
     );
